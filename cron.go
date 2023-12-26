cc038d99e67caf39e3a6513a563caa755d50df84
Marko Kungla <marko@mkungla.dev>
2023-12-26 16:44:23 +0200

    doc: SPDX-License-Identifier
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- cron.go
+++ cron.go
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The Happy Authors
+
 // Package cron is deprecated.
 //
 // This package has been deprecated and is no longer maintained.

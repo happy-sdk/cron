cf9070e5684ade28d48739f796d37c319976dfef
Marko Kungla <marko@mkungla.dev>
2023-12-26 17:22:10 +0200

    doc: deprecate github.com/happy-sdk/cron
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- cron.go
+++ cron.go
@@ -3,3 +3,21 @@
 // Copyright © 2023 The Happy Authors
 
 package cron
+
+import "time"
+
+const (
+	// Deprecated is a marker for deprecated code.
+	Deprecated = true
+
+	// DeprecatedBy is the name entity who deprecated this package.
+	DeprecatedBy = "The Happy Authors"
+
+	// NewLocation is the new location of this package.
+	NewLocation = "github.com/happy-sdk/happy-go/scheduling/cron"
+)
+
+// DeprecatedAt is the date when this package was deprecated.
+func DeprecatedAt() time.Time {
+	return time.Date(2023, time.December, 26, 17, 20, 0, 0, time.UTC)
+}

81b98d249bc16fb4a3bfc70a535fc55df4b4dba6
Marko Kungla <marko@mkungla.dev>
2023-12-26 16:39:06 +0200

    deprecaate module
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Package cron has been moved to [happy-sdk/happy-go/scheduling/cron](https://github.com/happy-sdk/happy-go/tree/main/scheduling/cron)
 
+[![PkgGoDev](https://pkg.go.dev/badge/github.com/happy-sdk/cron)](https://pkg.go.dev/github.com/happy-sdk/cron)
---
+++ cron.go
@@ -0,0 +1,7 @@
+// Package cron is deprecated.
+//
+// This package has been deprecated and is no longer maintained.
+// Please use the new package located at github.com/happy-sdk/happy-go/scheduling/cron.
+//
+// Deprecated: Use github.com/happy-sdk/happy-go/scheduling/cron instead.
+package cron

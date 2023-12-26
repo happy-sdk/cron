0d6442a357043c5e0e2925e1cf8f7226338e0741
Marko Kungla <marko@mkungla.dev>
2023-12-26 19:10:20 +0200

    doc: deprecate github.com/happy-sdk/cron
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- cron.go
+++ cron.go
@@ -23,5 +23,5 @@ const (
 
 // DeprecatedAt is the date when this package was deprecated.
 func DeprecatedAt() time.Time {
-	return time.Date(2023, time.December, 26, 17, 20, 0, 0, time.UTC)
+	return time.Date(2023, time.December, 26, 19, 00, 0, 0, time.UTC)
 }
